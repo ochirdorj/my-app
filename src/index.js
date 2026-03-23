1b47e81356ec5a16156bd8263989b19f9ae1c11e
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-22 19:41:01 -0500

    version react fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // src/index.js
 const express = require('express');
 const healthRouter = require('./health');
-const { version } = require('react');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
